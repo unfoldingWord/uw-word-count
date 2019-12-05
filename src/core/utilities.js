1e84fd54a24299969f1b5fc4e2b15f2452a89ad3
Cecil New <cecil.new@gmail.com>
2019-12-05 18:08:48 -0500

    prep for usfm format

---
+++ src/core/utilities.js
@@ -0,0 +1,17 @@
+// utilities
+
+
+// function to convert map to object
+export const map_to_obj = ( mp => {
+    const ob = {};
+    mp.forEach((v,k) => {ob[k]=v});
+    return ob;
+});
+
+// function to convert object to a map
+const obj_to_map = ( ob => {
+    const mp = new Map();
+    Object.keys ( ob ).forEach (k => { mp.set(k, ob[k]) });
+    return mp;
+});
+
--- src/core/uw-word-count.js
+++ src/core/uw-word-count.js
@@ -1,10 +1,74 @@
+import usfmjs from 'usfm-js';
+import * as util from './utilities';
 
-// function to convert map to object
-const map_to_obj = ( mp => {
-    const ob = {};
-    mp.forEach((v,k) => {ob[k]=v});
-    return ob;
-});
+
+function process_tags(v3,words,level) {
+    for (var j=0; j < v3.length; j++) {
+        let children_map = util.obj_to_map(v3[j]);
+        if ( children_map.get("type") === "word" ) {
+            let thisword = children_map.get("text");
+            words.push(thisword.toLowerCase());
+            continue;
+        }
+        let children = children_map.get("children");
+        if ( children !== undefined ) {
+            // Ok, we have a lower level list of children properties
+            // we recurse to pick them up
+            process_tags(children,words,level+1);
+        }
+    }
+}
+
+// get words from USFM format
+function getUsfmWords(usfm) {
+    // first convert to JSON
+    const json = usfmjs.toJSON(usfm);
+    const chapters = json.chapters;
+    let book_map = util.obj_to_map(chapters);
+    let words = [];
+
+    for (var [k,v] of book_map.entries()) {
+        // the value is a verses object 
+        // where key is verse number
+        // and value is an array of verse objects
+        var verses_map = util.obj_to_map(v);
+        //for (var [k1,v1] of verses_map.entries()) {
+        for (var v1 of verses_map.values()) {
+                //if ( k1 === "front" ) continue;
+            //console.log(". Working on verse:"+k1);
+            // the value is a set of tags for each object in a verse
+            var verse_map = obj_to_map(v1);
+            for (var v2 of verse_map.values()) {
+                for (var i=0; i < v2.length; i++) {
+                    var verse_obj_map = obj_to_map(v2[i]);
+                    // unaligned text method
+                    if ( verse_obj_map.has("text") ) {
+                        words.push(
+                            verse_obj_map.get("text").toLowerCase()
+                        );
+                    }
+                    // aligned text method
+                    if ( verse_obj_map.get("type") === "word" ) {
+                        let thisword = verse_obj_map.get("text");
+                        let lowerCaseVal = thisword.toLowerCase();
+                        words.push(lowerCaseVal);
+                    }
+                    for ( var [k3,v3] of verse_obj_map.entries()) {
+                        if ( k3 === "children" ) {
+                            process_tags(v3,words,1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // return the array of all words found
+    // in the order they were found
+    return words;
+}
+
+
+
     
 
 // Find words in Markdown text
@@ -117,7 +181,7 @@ export function wordCount(str,format) {
         w += 1;
         wordFrequency_map.set(allWords[i],w);
     }
-    let wordFrequency = map_to_obj(wordFrequency_map);
+    let wordFrequency = util.map_to_obj(wordFrequency_map);
     counts["wordFrequency"] = wordFrequency;
     return counts;
 }
